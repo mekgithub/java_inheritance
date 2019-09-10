13f49c3f55fd0358060182952fe120ec39682d43
MEK <54605592+mekgithub@users.noreply.github.com>
2019-09-10 17:57:35 -0500

    Leap Year Java Code
    added java code

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+package com;
+
+public class LeapYear {
+    public static void main(String[]args){
+       // Scanner input=new (System.in);
+        System.out.println("    Please enter the year");
+
+       // int remainder1=year % 4;
+        // int remainder2=year % 400;
+      //  int remainder3=year & 100;
+
+       // if (remainder3==0){
+           // if(remainder2==0) {
+                System.out.println("This is Leap year");
+            }
+          //  else {
+             //   System.out.println("not");
+            }
+
